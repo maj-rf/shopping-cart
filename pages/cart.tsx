be6489b7d649bc95a071f8f503f77c3e59708c4d
bananabread08 <rmkfermin@gmail.com>
2022-09-02 14:29:05 +0800

    remove Layout component

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -4,12 +4,12 @@ import AppHead from '../components/AppHead';
 
 const Cart: NextPage = () => {
   return (
-    <Layout>
+    <>
       <AppHead title="Cart" content="Cart Page of Switcher" />
       <section>
         <h1>Cart: 0</h1>
       </section>
-    </Layout>
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,12 @@ import AppHead from '../components/AppHead';
 
 const Home: NextPage = () => {
   return (
-    <Layout>
+    <>
       <AppHead title="Home" content="Switcher Shopping Cart" />
       <section>
         <h1>Switcher</h1>
       </section>
-    </Layout>
+    </>
   );
 };
 
