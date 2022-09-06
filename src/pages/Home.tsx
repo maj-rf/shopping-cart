c759c030c692d64a39fc8671f0695283f06db8f6
bananabread08 <rmkfermin@gmail.com>
2022-09-06 15:28:43 +0800

    modify Home using css modules

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
+import { Hero } from '../components/Hero';
+import util_styles from '../styles/utils.module.css';
+
 export const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={util_styles.container}>
+      <h1>Hello</h1>
+      <Hero />
     </div>
   );
 };
