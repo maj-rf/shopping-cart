f1a38d1a2fc9b572216d2471d0fe8dd2b7074676
bananabread08 <rmkfermin@gmail.com>
2022-09-08 17:28:46 +0800

    create Feature component with styles and assets

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { Hero } from '../components/Hero';
 import { Footer } from '../components/Footer';
+import { Featured } from '../components/Featured';
 
 export const Home = () => {
   return (
     <div>
       <Hero />
+      <Featured />
       <Footer />
     </div>
   );
