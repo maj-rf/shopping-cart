4a4a2092576b52d34f0b50996d540c809e3956ed
bananabread08 <rmkfermin@gmail.com>
2022-09-08 15:27:48 +0800

    restyle Hero and footer

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import utils from '../styles/utils.module.css';
 export const Hero = () => {
   return (
     <section className={utils.container}>
-      <h1>Hero</h1>
+      <h1>Toss a coin to your Switcher!</h1>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non
         repellendus debitis tenetur dolorem sunt molestias, minus qui
