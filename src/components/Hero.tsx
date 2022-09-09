30d391113246dd3b9eff001341192f689cb64f6d
bananabread08 <rmkfermin@gmail.com>
2022-09-09 10:22:02 +0800

    add image to hero, style button and heading

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,25 @@
 import utils from '../styles/utils.module.css';
+import styles from '../styles/hero.module.css';
+import heroImg from '../assets/play-games.png';
+import { Link } from 'react-router-dom';
 export const Hero = () => {
   return (
     <section className={utils.container}>
-      <h1>Toss a coin to your Switcher!</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non
-        repellendus debitis tenetur dolorem sunt molestias, minus qui
-        exercitationem placeat tempora provident natus, deleniti delectus!
-        Perspiciatis numquam eligendi iure tenetur odio debitis assumenda!
-        Assumenda, temporibus natus nemo exercitationem, officiis at similique
-        ab fuga dolorem recusandae unde ducimus iure, aperiam quae soluta!
-      </p>
-      <button>Shop Now</button>
+      <div className={styles.hero_container}>
+        <img src={heroImg} alt="guy playing games on tv" />
+        <div>
+          <h1 className={styles.hero_heading}>Toss a coin to your Switcher!</h1>
+          <p>
+            Switcher is your one-stop online Shop dedicated for Nintendo Switch
+            Products. Be up-to-date with the latest Switch games and
+            accessories. Pick, Add to Cart, and products will be delivered{' '}
+            <s>magically</s> at your doorsteps. Get Switched!
+          </p>
+        </div>
+      </div>
+      <button className={styles.hero_button}>
+        <Link to="/shop">Shop Now</Link>
+      </button>
     </section>
   );
 };
