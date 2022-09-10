fc390754ff6276f475f7a75e232a464daee92911
bananabread08 <rmkfermin@gmail.com>
2022-09-10 15:37:21 +0800

    create styles for CartItem

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,26 +1,29 @@
 import { data } from '../data/data';
 import { useShoppingCart } from './context/CartContext';
 import { CartItemType } from './types/type';
+import styles from '../styles/cartitem.module.css';
 
 export const CartItem = ({ id, quantity }: CartItemType) => {
   const { increaseQuantity, decreaseQuantity } = useShoppingCart();
   const item = data.find((x) => x.id === id);
   if (item == null) return null;
   return (
-    <div>
-      <img src={item.image} alt={item.name} />
-      <p>{item.name}</p>
-      <span>{quantity}</span>
-      <form>
-        <button type="button" onClick={() => increaseQuantity(id)}>
-          {' '}
-          +{' '}
-        </button>{' '}
-        <button type="button" onClick={() => decreaseQuantity(id)}>
-          {' '}
-          -{' '}
-        </button>
-      </form>
+    <div className={styles.cart_item}>
+      <img className={styles.item_img} src={item.image} alt={item.name} />
+      <div>
+        <p>{item.name}</p>
+        <span>{quantity}</span>
+        <form>
+          <button type="button" onClick={() => increaseQuantity(id)}>
+            {' '}
+            +{' '}
+          </button>{' '}
+          <button type="button" onClick={() => decreaseQuantity(id)}>
+            {' '}
+            -{' '}
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
