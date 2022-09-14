2e43fc2fa155180ac5c0d27184aea20f2ebafb8d
bananabread08 <rmkfermin@gmail.com>
2022-09-14 10:29:08 +0800

    move getSubtotal function to its own file and create test

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,30 +1,15 @@
 import { CartItem } from '../components/CartItem';
 import { useShoppingCart } from '../components/context/CartContext';
 import utils from '../styles/utils.module.css';
-import { data } from '../data/data';
-import { ShopItem } from '../components/types/type';
+import { getSubtotal } from '../components/utils/getSubtotal';
 export const Cart = (): JSX.Element => {
   const { cartItems } = useShoppingCart();
-
-  function getSubtotal() {
-    let total = 0;
-    if (!cartItems) return total;
-    const items = cartItems.map((a) => {
-      let obj = data.find((x) => x.id === a.id);
-      return { ...obj, quantity: a.quantity } as ShopItem;
-    });
-
-    for (let item of items) {
-      total = total + item.quantity * item.price;
-    }
-    return total;
-  }
 
   return (
     <div className={utils.container}>
       <h1>Your Shopping Cart</h1>
       <p>Number of Items: {cartItems.length}</p>
-      <p>Subtotal: {getSubtotal()}</p>
+      <p>Subtotal: {getSubtotal(cartItems)}</p>
       <div>
         {cartItems.map((item) => {
           return (
